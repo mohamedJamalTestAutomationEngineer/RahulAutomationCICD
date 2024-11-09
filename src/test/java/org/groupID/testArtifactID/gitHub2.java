2d7a37a067097985ac3c0943bf521d49af7140b2
Mohamed <engmjamal92@gmail.com>
2024-11-09 16:15:08 +0200

    first commit

---
+++ src/test/java/org/groupID/testArtifactID/gitHub2.java
@@ -0,0 +1,14 @@
+package org.groupID.testArtifactID;
+
+import org.testng.annotations.Test;
+
+import testingComponents.BaseTest;
+
+public class gitHub2 extends BaseTest{
+	
+	@Test
+	public void learnGitHub() {
+		System.out.println("this is for learning gitHub");
+	}
+
+}
