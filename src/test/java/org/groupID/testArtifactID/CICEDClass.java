cb20daca0c1fcaae394d5c629618c9e9edea9d2b
Mohamed <engmjamal92@gmail.com>
2024-11-11 16:16:21 +0200

    test Method added to check CICD

---
+++ src/test/java/org/groupID/testArtifactID/CICEDClass.java
@@ -0,0 +1,11 @@
+package org.groupID.testArtifactID;
+
+import org.testng.annotations.Test;
+
+public class CICEDClass {
+
+	@Test
+	public void testMethodCICD() {
+		System.out.println("this is for CI CD");
+	}
+}
