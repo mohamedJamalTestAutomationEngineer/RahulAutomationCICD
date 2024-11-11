9d29be24dccd033266220d90e3dd07ec39320f83
Mohamed <engmjamal92@gmail.com>
2024-11-11 16:24:01 +0200

    test CICD part 2

---
+++ src/test/java/org/groupID/testArtifactID/CICEDClass2.java
@@ -0,0 +1,11 @@
+package org.groupID.testArtifactID;
+
+import org.testng.annotations.Test;
+
+public class CICEDClass2 {
+
+	@Test
+	public void testMethodCICD() {
+		System.out.println("this is for CI CD   part 2");
+	}
+}
